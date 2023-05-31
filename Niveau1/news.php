be13cddcac061032e73444bb2c287889811c43f2
HanaaBelle <hanaabelhamidi@gmail.com>
2023-05-31 20:31:57 +0200

    First @todo: AREMPLACER in news.php accomplished

--- Niveau1/news.php
+++ Niveau1/news.php
@@ -102,13 +102,18 @@
                         <h3>
                             <time><?php echo $post['created'] ?></time>
                         </h3>
-                        <address>AREMPLACER</address>
+                        <address><?php echo $post['author_name'] ?></address>
                         <div>
-                            <p>AREMPLACER</p>
+                            <p><?php echo $post['content'] ?></p>
                         </div>
                         <footer>
-                            <small>♥ AREMPLACER </small>
-                            <a href="">AREMPLACER</a>,
+                            <small>♥<?php echo $post['like_number'] ?></small>
+                            <?php 
+                            $tagArray = explode(",",$post['taglist']);
+                                foreach ($tagArray as $tagElement){
+                                    ?><a href=""><?php echo "#" . $tagElement . ", ";
+                                }?>
+                             </a>
                         </footer>
                     </article>
                     <?php
