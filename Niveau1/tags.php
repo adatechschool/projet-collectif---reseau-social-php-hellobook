2d95ac3ac8bed9f5b3d2b4a62dfa5f0d513c6726
HanaaBelle <hanaabelhamidi@gmail.com>
2023-06-01 15:46:47 +0200

    Résolution des @todos sur tags.php

--- Niveau1/tags.php
+++ Niveau1/tags.php
@@ -33,8 +33,8 @@
                 <section>
                     <h3>Présentation</h3>
                     <p>Sur cette page vous trouverez les derniers messages comportant
-                        le mot-clé XXX
-                        (n° <?php echo $tagId ?>)
+                        le mot-clé : 
+                        <?php echo $tag['label'] ?>
                     </p>
 
                 </section>
@@ -76,20 +76,20 @@
                     ?>                
                     <article>
                         <h3>
-                            <time datetime='2020-02-01 11:12:13' >31 février 2010 à 11h12</time>
+                            <time><?php echo $post['created'] ?></time>
                         </h3>
-                        <address>par AreTirer</address>
+                        <address><?php echo $post["author_name"] ?></address>
                         <div>
-                            <p>Ceci est un paragraphe</p>
-                            <p>Ceci est un autre paragraphe</p>
-                            <p>... de toutes manières il faut supprimer cet 
-                                article et le remplacer par des informations en 
-                                provenance de la base de donnée</p>
+                            <p><?php echo $post["content"] ?></p>
                         </div>                                            
                         <footer>
-                            <small>♥ 132</small>
-                            <a href="">#lorem</a>,
-                            <a href="">#piscitur</a>,
+                        <small>♥<?php echo $post['like_number'] ?></small>
+                            <?php 
+                            $tagArray = explode(",",$post['taglist']);
+                                foreach ($tagArray as $tagElement){
+                                    ?><a href=""><?php echo "#" . $tagElement . ", ";
+                                }?>
+                                </a>
                         </footer>
                     </article>
                 <?php } ?>
