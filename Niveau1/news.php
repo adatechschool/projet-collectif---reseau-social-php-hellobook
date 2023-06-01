57a25829333cb6a0687ca547f22b2c180283fe7d
Coquelicot49 <coraline.jadeau@gmail.com>
2023-06-01 10:24:40 +0200

    exercice page news gestion de conflit

--- Niveau1/news.php
+++ Niveau1/news.php
@@ -103,8 +103,10 @@
                             <time><?php echo $post['created'] ?></time>
                         </h3>
                         <address><?php echo $post['author_name'] ?></address>
+                        <address><?php echo $post["author_name"] ?></address>
                         <div>
                             <p><?php echo $post['content'] ?></p>
+                            <p><?php echo $post["content"] ?></p>
                         </div>
                         <footer>
                             <small>♥<?php echo $post['like_number'] ?></small>
